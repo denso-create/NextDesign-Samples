65d7bbfd1621712f68a97ad1b99aeae24099226c
TAJIMA, Keiichi <keiichi.tajima.j8y@jpgr.denso.com>
2024-03-22 20:40:21 +0900

    extensions/GenerateUiTestCases 処理内容更新

--- extensions/GenerateUiTestCases/GenerateUiTestCases/GenerateUiTestCasesEntryPoint.cs
+++ extensions/GenerateUiTestCases/GenerateUiTestCases/GenerateUiTestCasesEntryPoint.cs
@@ -49,39 +49,48 @@ namespace GenerateUiTestCases
         {
             // 前提条件を確認します。
             var project = App.Workspace.CurrentProject;
-            var uiModel = App.Window.EditorPage?.CurrentEditorView?.Editor?.Model;
-            if ((uiModel == null) || (! uiModel.As("UiWindow")))
+            var currentModel = App.Window.EditorPage?.CurrentEditorView?.Editor?.Model;
+            if (currentModel == null)
             {
-                App.Window.UI.ShowMessageBox("対象のUI画面を表示させてから実行してください。");
+                App.Window.UI.ShowMessageBox("対象モデルを表示させてから実行してください。");
                 return;
             }
-            var testGroupModel = project.FindChildrenByClass("TestGroup", recursive: true).FirstOrDefault();
-            if (testGroupModel == null)
+            var testSpec = project.FindChildrenByClass("TestSpec", recursive: true).FirstOrDefault();
+            if (testSpec == null)
             {
-                App.Window.UI.ShowMessageBox("テストグループを作成してから実行してください。");
+                App.Window.UI.ShowMessageBox("テスト仕様モデルを作成してから実行してください。");
                 return;
             }
-
-            // モデルエディタに表示されているUI画面の中からUI部品の操作仕様を抽出します。
-            var operationModels = uiModel.FindChildrenByClass("OperationSpec", recursive: true);
-
-            // 操作仕様に対応したテストケースを作成します。
-            foreach (var operationModel in operationModels)
+            
+            // UI画面ごとにテストグループを作成して、テストケースを追加します。
+            var uiWindows = currentModel.FindChildrenByClass("UiWindow", recursive: true);
+            foreach (var uiWindow in uiWindows)
             {
-                var testCaseModel = testGroupModel.AddNewModel("TestCases", "TestCase");
+                // UI画面ごとにテストグループを作成します。
+                var testGroupModel = testSpec.AddNewModel("TestGroups", "TestGroup");
+                testGroupModel.SetField("Name", uiWindow.Name);
                 
-                // 操作仕様に基づいてテストケースの名前と期待値を設定します。
-                var uiElementName = operationModel.Owner.Name;          // UI部品の名前
-                var operation = operationModel.Name;                    // 操作
-                var action = operationModel.GetFieldString("Action");   // 動作
-
-                var testCaseLabel = $"{uiElementName}・{operation}";    // テストケースの名前
-                var expectedValue = $"{uiElementName}が{operation}されると、{action}";  // 期待値
-                testCaseModel.SetField("Name", testCaseLabel);
-                testCaseModel.SetField("ExpectedValue", expectedValue);
-
-                // 操作仕様とテストケース間にトレース関連（導出関連）を結びます。
-                testCaseModel.Relate("input_OperationSpec", operationModel);
+                // UI画面の中からUI部品の操作仕様を抽出します。
+                var operationModels = uiWindow.FindChildrenByClass("OperationSpec", recursive: true);
+                
+                // 操作仕様に対応したテストケースを作成します。
+                foreach (var operationModel in operationModels)
+                {
+                    var testCaseModel = testGroupModel.AddNewModel("TestCases", "TestCase");
+                    
+                    // 操作仕様に基づいてテストケースの名前と期待値を設定します。
+                    var uiElementName = operationModel.Owner.Name;          // UI部品の名前
+                    var operation = operationModel.Name;                    // 操作
+                    var action = operationModel.GetFieldString("Action");   // 動作
+            
+                    var testCaseLabel = $"{uiElementName}・{operation}";    // テストケースの名前
+                    var expectedValue = $"{uiElementName}が{operation}されると、{action}";  // 期待値
+                    testCaseModel.SetField("Name", testCaseLabel);
+                    testCaseModel.SetField("ExpectedValue", expectedValue);
+            
+                    // 操作仕様とテストケース間にトレース関連（導出関連）を結びます。
+                    testCaseModel.Relate("input_OperationSpec", operationModel);
+                }
             }
         }
 
